9d452f2e2ca569c2f8aacdc9df5dc7009de171c4
Silva <andrew.silva@atos.net>
2022-07-02 23:25:57 -0300

    Queries & Front-End Data Fetching

--- server/index.js
+++ server/index.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const colors = require('colors')
-require('dotenv').config()
-const {graphqlHTTP} = require('express-graphql')
-const schema = require('./schema/schema')
-const connectDB = require('./config/db')
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 //Connect to Database
-connectDB()
+connectDB();
 
-app.use('/graphql', graphqlHTTP({
+//Middleware
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development',
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
-app.listen(port, console.log(`Server running on port ${port}`))
+app.listen(port, console.log(`Server running on port ${port}`));
