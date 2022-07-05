c5d866ff45ac88b825096d92c7c18d5c17fbc79d
Silva <andrew.silva@atos.net>
2022-07-05 19:57:59 -0300

    Delete Projects

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,10 @@
 import { Link, useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 
+import Spinner from "../components/Spinner";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 
 export default function Project() {
   const { id } = useParams();
@@ -30,6 +31,8 @@ export default function Project() {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
